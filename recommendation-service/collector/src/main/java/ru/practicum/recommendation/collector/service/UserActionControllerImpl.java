8e7b8f3b93be965702a1317845e2b7bdcbdfb241
flytomars <tramp.box@gmail.com>
2026-05-25 01:46:58 +0300

    приведение сериализатора к json

--- recommendation-service/collector/src/main/java/ru/practicum/recommendation/collector/service/UserActionControllerImpl.java
+++ recommendation-service/collector/src/main/java/ru/practicum/recommendation/collector/service/UserActionControllerImpl.java
@@ -3,30 +3,27 @@ package ru.practicum.recommendation.collector.service;
 import ru.practicum.ewm.stats.proto.Empty;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.apache.avro.io.BinaryEncoder;
-import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.specific.SpecificDatumWriter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import ru.practicum.ewm.stats.avro.ActionTypeAvro;
-import ru.practicum.ewm.stats.avro.UserActionAvro;
 import ru.practicum.ewm.stats.proto.ActionTypeProto;
 import ru.practicum.ewm.stats.proto.UserActionProto;
 import ru.practicum.ewm.stats.proto.UserActionControllerGrpc;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @GrpcService
 public class UserActionControllerImpl extends UserActionControllerGrpc.UserActionControllerImplBase {
 
     private static final Logger log = LoggerFactory.getLogger(UserActionControllerImpl.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
-    private KafkaTemplate<String, byte[]> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
     public void collectUserAction(UserActionProto request, StreamObserver<Empty> responseObserver) {
@@ -34,20 +31,19 @@ public class UserActionControllerImpl extends UserActionControllerGrpc.UserActio
                 request.getUserId(), request.getEventId(), request.getActionType(), request.getTimestamp());
 
         try {
-            UserActionAvro avroMessage = UserActionAvro.newBuilder()
-                    .setUserId(request.getUserId())
-                    .setEventId(request.getEventId())
-                    .setActionType(convertActionType(request.getActionType()))
-                    .setTimestamp(Instant.ofEpochMilli(request.getTimestamp()))
-                    .build();
+            Map<String, Object> jsonMessage = new HashMap<>();
+            jsonMessage.put("userId", request.getUserId());
+            jsonMessage.put("eventId", request.getEventId());
+            jsonMessage.put("actionType", request.getActionType().toString());
+            jsonMessage.put("timestamp", request.getTimestamp());
 
-            byte[] data = serialize(avroMessage);
+            String json = objectMapper.writeValueAsString(jsonMessage);
 
             kafkaTemplate.send("stats.user-actions.v1",
                     String.valueOf(request.getUserId()),
-                    data);
+                    json);
 
-            log.debug("Sent to Kafka: {}", avroMessage);
+            log.debug("Sent to Kafka: {}", json);
 
             responseObserver.onNext(Empty.getDefaultInstance());
             responseObserver.onCompleted();
@@ -55,29 +51,6 @@ public class UserActionControllerImpl extends UserActionControllerGrpc.UserActio
         } catch (Exception e) {
             log.error("Error processing user action", e);
             responseObserver.onError(e);
-        }
-    }
-
-    private byte[] serialize(UserActionAvro data) throws IOException {
-        SpecificDatumWriter<UserActionAvro> writer = new SpecificDatumWriter<>(UserActionAvro.getClassSchema());
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
-        writer.write(data, encoder);
-        encoder.flush();
-        return out.toByteArray();
-    }
-
-    private ActionTypeAvro convertActionType(ActionTypeProto actionType) {
-        switch (actionType) {
-            case ACTION_VIEW:
-                return ActionTypeAvro.VIEW;
-            case ACTION_REGISTER:
-                return ActionTypeAvro.REGISTER;
-            case ACTION_LIKE:
-                return ActionTypeAvro.LIKE;
-            default:
-                log.warn("Unknown action type: {}, defaulting to VIEW", actionType);
-                return ActionTypeAvro.VIEW;
         }
     }
 }
